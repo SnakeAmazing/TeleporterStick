f359d86e52d3eb526e9d9b7d44fe46f79f59381f
SnakeAmazing <68821487+SnakeAmazing@users.noreply.github.com>
2020-11-26 20:42:03 +0100

    asd

--- src/main/java/me/snakeamazing/listeners/OnPlayerInteract.java
+++ src/main/java/me/snakeamazing/listeners/OnPlayerInteract.java
@@ -19,7 +19,7 @@ public class OnPlayerInteract implements Listener {
     private FileManager config;
     private TeleporterStick plugin;
 
-    public OnPlayerInteract(FileMatcher matcher, TeleporterStick plugin){
+    public OnPlayerInteract(FileMatcher matcher, TeleporterStick plugin) {
         this.config = matcher.getFile("config");
         this.plugin = plugin;
     }
@@ -27,43 +27,43 @@ public class OnPlayerInteract implements Listener {
     public HashMap<UUID, Long> cooldownTimer = new HashMap<>();
 
     @EventHandler
-    public void onPlayerInteract(PlayerInteractEvent event){
+    public void onPlayerInteract(PlayerInteractEvent event) {
 
         Player player = event.getPlayer();
         if (player.getItemInHand().getType() == Material.STICK && event.getAction() == Action.RIGHT_CLICK_AIR) {
 
             boolean block = player.getWorld().getBlockAt(player.getEyeLocation().getBlock().getLocation()).getType().isSolid();
 
+            if (!cooldownTimer.containsKey(player.getUniqueId())) {
+                return;
+            }
 
-            if (cooldownTimer.containsKey(player.getUniqueId())) {
-                player.sendMessage(config.getString("messages.delay-message"));
+            if (block) {
+                player.sendMessage(config.getString("messages.prohibed"));
 
             } else {
-                if (block) {
-                    player.sendMessage(config.getString("messages.prohibed"));
+                Location loc = player.getLocation();
+                Vector vec = loc.getDirection();
+                vec.normalize();
+                vec.multiply(config.getInt("items.teleporter-stick.blocks-to-teleport"));
+                loc.add(vec);
 
+                if (!(loc.getBlock().getType().isSolid()) || loc.getBlock().getType().isTransparent()) {
+                    player.teleport(loc);
+                    player.playSound(player.getLocation(), Sound.ENDERMAN_TELEPORT, 10, 1);
+                    player.getWorld().playEffect(player.getLocation(), Effect.SMOKE, 50, 8);
+                    player.getWorld().playEffect(player.getEyeLocation(), Effect.INSTANT_SPELL, 50, 8);
+                    cooldownTimer.put(player.getUniqueId(), System.currentTimeMillis());
+                    setCooldown(3, player);
                 } else {
-                    Location loc = player.getLocation();
-                    Vector vec = loc.getDirection();
-                    vec.normalize();
-                    vec.multiply(config.getInt("items.teleporter-stick.blocks-to-teleport"));
-                    loc.add(vec);
-
-                    if (!(loc.getBlock().getType().isSolid()) || loc.getBlock().getType().isTransparent()){
-                        player.teleport(loc);
-                        player.playSound(player.getLocation(), Sound.ENDERMAN_TELEPORT, 1, 1);
-                        player.getWorld().playEffect(player.getLocation(), Effect.SMOKE, 50 ,8);
-                        player.getWorld().playEffect(player.getEyeLocation(), Effect.INSTANT_SPELL, 50, 8);
-                        cooldownTimer.put(player.getUniqueId(), System.currentTimeMillis());
-                        cooldown(3, player);
-                    } else {
-                        player.sendMessage(config.getString("messages.prohibed"));
-                    }
-                }
+                    player.sendMessage(config.getString("messages.prohibed"));
             }
         }
     }
-    public void cooldown(int seconds, Player player) {
+}
+
+
+    public void setCooldown(int seconds, Player player) {
         Bukkit.getServer().getScheduler().runTaskLaterAsynchronously(plugin, () -> {
             cooldownTimer.remove(player.getUniqueId());
         }, seconds * 20);
